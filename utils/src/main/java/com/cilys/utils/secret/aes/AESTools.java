352670855c782c500cba6db311d552fe9a60be44
cilys <1321851830@qq.com>
2021-11-11 13:14:34 +0800

    重新实现

--- utils/src/main/java/com/cilys/utils/secret/aes/AESTools.java
+++ utils/src/main/java/com/cilys/utils/secret/aes/AESTools.java
@@ -1,12 +1,12 @@
 package com.cilys.utils.secret.aes;
 
-import com.cilys.utils.secret.java.BASE64Decoder;
-import com.cilys.utils.secret.java.BASE64Encoder;
-
+import java.io.UnsupportedEncodingException;
+import java.security.SecureRandom;
 import java.util.Arrays;
 
 import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 public class AESTools implements EncryptStringImpl, DecryptStringImpl, EncryptByteImpl, DecryptByteImpl {
@@ -32,9 +32,9 @@ public class AESTools implements EncryptStringImpl, DecryptStringImpl, EncryptBy
         }
 
         try {
-            return new String(decrypt(new BASE64Decoder().decodeBuffer(data), pwd), CHARSET);
-        } catch (Exception e) {
-            if (debug) {
+            return new String(decrypt(parseHexStr2Byte(data), pwd), CHARSET);
+        } catch (UnsupportedEncodingException e) {
+            if (debug){
                 e.printStackTrace();
             }
             return null;
@@ -50,7 +50,7 @@ public class AESTools implements EncryptStringImpl, DecryptStringImpl, EncryptBy
             return null;
         }
         try {
-            return new BASE64Encoder().encode(encrypt(data.getBytes(CHARSET), pwd));
+            return parseByte2HexStr(encrypt(data.getBytes(CHARSET), pwd));
         } catch (Exception e) {
             if (debug) {
                 e.printStackTrace();
@@ -75,30 +75,21 @@ public class AESTools implements EncryptStringImpl, DecryptStringImpl, EncryptBy
             System.out.println("加密密钥：" + pwd);
         }
         try {
-            Cipher cipher = Cipher.getInstance("AES/CBC/NOPadding");   //参数分别代表 算法名称/加密模式/数据填充方式
-            int blockSize = cipher.getBlockSize();
-
-            int plaintextLength = dataBytes.length;
-            if (plaintextLength % blockSize != 0) {
-                plaintextLength = plaintextLength + (blockSize - (plaintextLength % blockSize));
-            }
-
-            byte[] plaintext = new byte[plaintextLength];
-            System.arraycopy(dataBytes, 0, plaintext, 0, dataBytes.length);
-
-            SecretKeySpec keyspec = new SecretKeySpec(pwd.getBytes(), "AES");
-            IvParameterSpec ivspec = new IvParameterSpec(pwd.getBytes());
-
-            cipher.init(Cipher.ENCRYPT_MODE, keyspec, ivspec);
-            byte[] encrypted = cipher.doFinal(plaintext);
-
-            return encrypted;
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(128, new SecureRandom(pwd.getBytes()));
+            SecretKey secretKey = kgen.generateKey();
+            byte[] enCodeFormat = secretKey.getEncoded();
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
+            cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
+            byte[] result = cipher.doFinal(dataBytes);
+            return result; // 加密
         } catch (Exception e) {
             if (debug) {
                 e.printStackTrace();
             }
-            return null;
         }
+        return null;
     }
 
     @Override
@@ -117,20 +108,59 @@ public class AESTools implements EncryptStringImpl, DecryptStringImpl, EncryptBy
             System.out.println("解密密钥：" + pwd);
         }
         try {
-
-            Cipher cipher = Cipher.getInstance("AES/CBC/NOPadding");
-            SecretKeySpec keyspec = new SecretKeySpec(pwd.getBytes(), "AES");
-            IvParameterSpec ivspec = new IvParameterSpec(pwd.getBytes());
-
-            cipher.init(Cipher.DECRYPT_MODE, keyspec, ivspec);
-
-            byte[] original = cipher.doFinal(byteDatas);
-            return original;
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(128, new SecureRandom(pwd.getBytes()));
+            SecretKey secretKey = kgen.generateKey();
+            byte[] enCodeFormat = secretKey.getEncoded();
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
+            cipher.init(Cipher.DECRYPT_MODE, key);// 初始化
+            byte[] result = cipher.doFinal(byteDatas);
+            return result; // 加密
         } catch (Exception e) {
-            if (debug) {
-                e.printStackTrace();
-            }
-            return null;
+            e.printStackTrace();
         }
+        return null;
     }
+
+    public String parseByte2HexStr(byte buf[]) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < buf.length; i++) {
+            String hex = Integer.toHexString(buf[i] & 0xFF);
+            if (hex.length() == 1) {
+                hex = '0' + hex;
+            }
+            sb.append(hex.toUpperCase());
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     * 将16进制转换为二进制
+     *
+     * @param hexStr
+     * @return
+     */
+    public byte[] parseHexStr2Byte(String hexStr) {
+        if (hexStr.length() < 1)
+            return null;
+        byte[] result = new byte[hexStr.length() / 2];
+        for (int i = 0; i < hexStr.length() / 2; i++) {
+            int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
+            int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2), 16);
+            result[i] = (byte) (high * 16 + low);
+        }
+        return result;
+    }
+
+//    public static void main(String[] args) {
+//        AESTools aesTools = new AESTools(true);
+//        AESTools aesSortTools = new AESSortTools(true);
+//        String str = "Hello，中国";
+//        String e = aesSortTools.encrypt(str, null);
+//        System.out.println("加密：" + e);
+//        String d = aesSortTools.decrypt(e, null);
+//        System.out.println("解密：" + d);
+//    }
 }
